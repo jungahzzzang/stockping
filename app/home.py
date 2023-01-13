421dddc456e94a5ff76417c00c8c93472c51aae5
Kim YeongHyeon <61192230+arcensia@users.noreply.github.com>
2023-01-13 14:45:31 +0900

    시가 총액 기준으로 main주식 데이터 변경 및 데이터 정렬 기능 분리

--- app/home.py
+++ app/home.py
@@ -1,11 +1,11 @@
+#run.py 기준 경로
 from app import *
-from flask import Blueprint, jsonify
-from bson.json_util import dumps
+from flask import Blueprint
+from app.stock import getTopTen as gt
+
 
 app = Flask(__name__)
-
 blueprint = Blueprint("home", __name__, url_prefix="/")
-
 abs_path = os.getcwd()
 
 with open(abs_path+'/app/settings/config.json', 'r') as f:
@@ -13,8 +13,11 @@ with open(abs_path+'/app/settings/config.json', 'r') as f:
     db_info = config['DB']
     api_info = config['NAVERAPI']
 
-    db_name = db_info['db_name']
-    collection_name = db_info['news_collection']
+    mongo_connect = db_info['MONGO_URI']
+    client = MongoClient(mongo_connect)
+    
+    db = client.db_info['db_name']
+    collection = db.db_info['collection_name']
 
 @blueprint.route('/main', methods=['GET', 'POST'])
 def index():
@@ -37,9 +40,8 @@ def index():
     target_res = requests.get(target_url)
     target_soup = bs(target_res.text, 'html.parser')
     data_rows = target_soup.find("table", attrs={"class":"type_2"}).find("tbody").find_all("tr")
-    data_index = target_soup.find("table", attrs={"class":"type_2"}).find("tbody").select("tr>td>a")
-    # print(data_index)
-    
+   
+   
     data = []
     for row in data_rows:
         # 각 row마다 td를 가지고옴
@@ -47,64 +49,22 @@ def index():
         # td가 1개인 것(데이터 없는 것)은 skip
         if len(columns)<=1:
             continue
-        # data = []
         top_stock = []
-        code_list = []
         for column in columns:
             origin = column.get_text().strip()
-            top_stock.append(origin)    
+            top_stock.append(origin)
+            
         del top_stock[5:]
         data.append(top_stock)
-        
-    #주식 코드 크롤링
-    stock_code_list = []
-    for index in data_index:
-        href = index.attrs['href']
-        stockcode = href[-6:]
-        print(stockcode)
-        stock_code_list.append(stockcode)
-    
-    number = 0
-    for i in data: # % 삭제
-        data[number][4] = (i[4][:-1])
-        number = number +1
-    # print(data)
 
+    topten = gt.get_topten(data)
 
-    ## 선택 정렬을 이용해 data변수 정렬
-    def selectionSort(x):
-        length = len(x)
-        for i in range(length-1):
-            indexMin = i
-            for j in range(i+1, length):
-                if float(x[indexMin][-1]) > float(x[j][-1]):
-                    indexMin = j
-            x[i], x[indexMin] = x[indexMin], x[i]
-        return x
-
-    data = selectionSort(data)
-    data.reverse()
-
-    topten = [] #10위까지 저장
-    for i in range(0,len(data)):
-        data[i][4] = data[i][4]+'%' 
-        if i < 10:
-            topten.append(data[i])
-            topten[i][0] = str(i+1)
-            
+         
     return render_template('index.html', real_time=real_time, kospi_value=kospi_value.text, kosdaq_value = kosdaq_value.text, kospi_change=kospi_change, kosdaq_change=kosdaq_change, top_stock=topten)
 
-@blueprint.route('/api/news')
-def send_news():
-    
-    client = MongoClient(db_info['MONGO_URI'])   
-    db = client[db_name]
-    collection = db[collection_name] 
-    data = list(collection.find({}).sort("pubDate",-1).limit(10)); # 최근 10개
-    news_data = []
-    for _data in data:
-        if _data not in news_data:
-            _data['_id'] = str(_data['_id'])
-            news_data.append(_data)
-    
-    return jsonify({"news":news_data})
+
+
+
+# @app.route('/')
+# def gotomain():
+#    return render_template('/main')
